6866162af2eab7159b9e6a735255b5717c111400
kinnicbj <77947023+rhit-kinnicbj@users.noreply.github.com>
2022-07-21 12:30:47 -0400

    working on header, sidebar animations

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,11 @@ function luxrayHandler() {
   let food = document.getElementById("food");
   let heart = document.getElementById("heart");
   let dropBtn = document.getElementById("drop");
+  let sidebar = document.querySelector('.sidebar')
   dropBtn.addEventListener("click", dropFood);
 
   let movementInterval;
-  let jumpInterval;
+  let jumpingInterval;
 
   startMoving(50, 25);
   startJumping();
@@ -17,14 +18,15 @@ function luxrayHandler() {
     }
 
     clearInterval(movementInterval);
-    clearInterval(jumpInterval);
+    clearInterval(jumpingInterval);
+    luxray.dataset.isMoving = false;
 
     let foodPos =
       window.innerHeight -
       dropBtn.getBoundingClientRect().y -
       dropBtn.offsetHeight;
     food.style.bottom = `${foodPos}px`;
-    food.style.left = `${Math.random() * (125 - 40) + 40}px`;
+    food.style.left = `${Math.random() * (sidebar.offsetWidth-110 - 40) + 40}px`;
     food.style.display = "block";
     console.log();
     let dropInterval = setInterval(() => {
@@ -70,9 +72,10 @@ function luxrayHandler() {
   }
 
   function startMoving(currentPos = 50, speed = 25) {
-    if (foodExists()) {
+    if (foodExists() || luxray.dataset.isMoving == "true") {
       return;
     }
+    luxray.dataset.isMoving = true;
     let luxDirection = Math.random() > 0.5 ? "left" : "right";
     if (luxDirection === "left") {
       luxray.style.transform = "scaleX(1)";
@@ -81,7 +84,8 @@ function luxrayHandler() {
     }
     let luxPos = currentPos;
     movementInterval = setInterval(async () => {
-      if (luxPos >= 170) {
+
+      if (luxPos >= sidebar.offsetWidth - 80) {
         luxDirection = "left";
         luxray.style.transform = "scaleX(1)";
       } else if (luxPos <= 10) {
@@ -93,6 +97,7 @@ function luxrayHandler() {
 
       if (Math.random() * 100 < 2) {
         clearInterval(movementInterval);
+        luxray.dataset.isMoving = false;
         wait();
       }
     }, speed);
@@ -102,28 +107,30 @@ function luxrayHandler() {
     let currentPos = parseInt(luxray.style.left.split("px")[0]);
     let speeds = [10, 25, 40];
     let speed = speeds[Math.floor(Math.random() * speeds.length)];
-    if (isJumping()) {
+    if (luxray.dataset.isJumping == "true") {
       startMoving(currentPos, speed);
       return;
     }
-    clearInterval(movementInterval);
+    clearInterval(movementInterval); 
+    luxray.dataset.isMoving = false;
     setTimeout(() => {
       startMoving(currentPos, speed);
     }, 2000);
   }
 
   function startJumping() {
-    jumpInterval = setInterval(() => {
+    jumpingInterval = setInterval(() => {
       if (Math.random() * 10 > 6) {
         jump();
       }
-    }, 2500);
+    }, 2000);
   }
 
   function jump() {
-    if (isJumping()) {
+    if (luxray.dataset.isJumping == "true") {
       return;
     }
+    luxray.dataset.isJumping = "true"
     let height = 0;
     let dir = "up";
     let jumpInterval = setInterval(() => {
@@ -131,18 +138,17 @@ function luxrayHandler() {
         dir = "down";
       } else if (height == 0 && dir === "down") {
         clearInterval(jumpInterval);
+        luxray.dataset.isJumping = "false" 
+        if (Math.random() * 10 > 7) {
+          setTimeout(jump,20);
+        }
       }
       luxray.style.bottom = `${height}px`;
       height = dir === "up" ? height + 2 : height - 2;
     }, 15);
-    if (Math.random() * 10 > 5) {
-      jump();
-    }
+
   }
 
-  function isJumping() {
-    return luxray.style.bottom && luxray.style.bottom != "0px";
-  }
 
   function foodExists() {
     return food.style.display && food.style.display != "none";
@@ -155,6 +161,9 @@ function navHandler() {
   window.onresize = () => {
     if (window.innerWidth > 699) {
       document.querySelector(".sidebar").style.display = "block";
+    }else{
+      document.querySelector(".sidebar").style.display = "none";
+
     }
   };
 
